ab51664fdf8a017c6ce41a9a8ded7a92046661d7
ROK <roking2000@konkuk.ac.kr>
2026-01-31 19:20:26 +0900

    test

---
+++ bits/stdc++.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <list>
+// 필요한 헤더들을 여기에 다 넣어두면 됩니다.
