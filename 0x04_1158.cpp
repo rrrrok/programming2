d34e3f99ce914b7645a206ce9efd46f83fe0d1da
rrrrok <roking2000@konkuk.ac.kr>
2026-02-03 00:13:40 +0900

    _1158

---
+++ 0x04_1158.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    return 0;
+    // int N,K;
+    // cin >> N >> K;
+
+    // list<int> L;
+    // list<int>::iterator iter = L.begin();
+
+    // cout <<'<';
+    // while(1){
+    //     iter = (iter + (K-1))%L.size();
+    //     cout << *iter;
+    //     iter = L.erase(iter);
+
+    //     if(L.empty()){
+    //         break;
+    //     }
+    // }
+    // cout <<'>';
+
+    // return 0;
+}
