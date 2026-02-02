b164ac84cfc060dd3136586054df5d0efc5db5cb
rrrrok <roking2000@konkuk.ac.kr>
2026-02-03 00:40:54 +0900

    _1158

--- 0x04_1158.cpp
+++ 0x04_1158.cpp
@@ -2,25 +2,39 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, World!" << endl;
+    int N,K;
+    cin >> N >> K;
+
+    list<int> L;
+    for (int i=1;i<=N;i++){
+        L.push_back(i);
+    }
+
+    list<int>::iterator iter = L.begin();
+
+    cout <<'<';
+    while(1){
+        for (int i=0; i<K-1;i++){
+            iter++;
+            if(iter ==L.end()){
+                iter = L.begin();
+            }
+        }
+        cout << *iter;
+        iter = L.erase(iter);
+
+        if(iter == L.end()){
+            iter = L.begin();
+        }
+        if(!L.empty()){
+            cout <<", ";
+        }
+
+        if(L.empty()){
+            break;
+        }
+    }
+    cout <<'>';
+
     return 0;
-    // int N,K;
-    // cin >> N >> K;
-
-    // list<int> L;
-    // list<int>::iterator iter = L.begin();
-
-    // cout <<'<';
-    // while(1){
-    //     iter = (iter + (K-1))%L.size();
-    //     cout << *iter;
-    //     iter = L.erase(iter);
-
-    //     if(L.empty()){
-    //         break;
-    //     }
-    // }
-    // cout <<'>';
-
-    // return 0;
 }
